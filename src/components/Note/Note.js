d958153e293699126bb9b7c96ae3eea68656f870
Ivan Samardzic <isamar01@fesb.hr>
2021-03-11 16:18:56 +0100

    Edit Note compo

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Note.css'
 
-const Note = ({note}) => {
+const Note = ({note, deleteNote}) => {
     return (
         <div className='note-container' key={note.id}>
             <i className="fas fa-exclamation-circle"></i>
             <div className='note-title'>{note.title}</div>
             <div className='note-date'>{note.date}</div> 
             <div className='note-text'>{note.text}</div>
-            <button className="note-btn btn btn-light"><i className="far fa-trash-alt"></i></button>
+            <button onClick={() => deleteNote(note)} className="note-btn btn btn-light"><i className="far fa-trash-alt"></i></button>
         </div>
     )
 }
