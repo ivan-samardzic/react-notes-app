2f8b418b921f03cfe612ce3ced4abde879aa4c22
Ivan Samardzic <isamar01@fesb.hr>
2021-03-11 16:19:11 +0100

    Edit Notes compo

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -2,11 +2,11 @@ import React from 'react'
 import './Notes.css'
 import Note from '../Note/Note'
 
-const Notes = ({notes}) => {
+const Notes = ({notes, deleteNote}) => {
     return (
         <div className='notes-box'>
             {notes.map((note, index) => 
-                <Note note={note} />
+                <Note note={note}  deleteNote={deleteNote} />
             )}
         </div>
     )
