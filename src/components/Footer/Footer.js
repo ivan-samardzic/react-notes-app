6ed39bdc14bf05005637989b9d5818857445138e
Ivan Samardzic <isamar01@fesb.hr>
2021-03-11 16:19:29 +0100

    Add simple Footer compo

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>2021 | Notes React App</p>
+        </footer>
+    )
+}
+
+export default Footer
