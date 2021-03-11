6df4b6e3f42f63cc4118f5a8c2ea17c8b4150007
Ivan Samardzic <isamar01@fesb.hr>
2021-03-11 18:37:04 +0100

    Add an reference to NoteContext and consume addNote method logic

--- src/components/NewNote/NewNote.js
+++ src/components/NewNote/NewNote.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState, useContext} from 'react'
 import './NewNote.css'
+import NoteContext from '../../context/noteContext'
 
-const NewNote = ({addNote}) => {
+const NewNote = (props) => {
+    const context = useContext(NoteContext)
     const [title, setTitle] = useState('')
     const [date, setDate] = useState('')
     const [text, setText] = useState('')
@@ -10,7 +12,7 @@ const NewNote = ({addNote}) => {
     const onSubmit= (event) => {
         event.preventDefault();
 
-        addNote({title, text, date});
+        context.addNote({title, text, date});
 
         setTitle('')
         setDate('')
@@ -18,24 +20,28 @@ const NewNote = ({addNote}) => {
     }
 
     return (
-        <div className="new-note-container">
-            <h3 className="new-note-title">Add New Note</h3>
-            <form className="add-form-container m-3" onSubmit={onSubmit}>
-                <div className="form-group">
-                    <label>Title</label>
-                    <input type="text" placeholder="Note Title" value={title} onChange={(event) => setTitle(event.target.value)}/>
-                </div>
-                <div className="form-group">
-                    <label>Date</label>
-                    <input type="text" placeholder="Node Date" value={date} onChange={(event) => setDate(event.target.value)}/>
-                </div>
-                <div className="form-group">
-                    <label>Text</label>
-                    <textarea type="text" placeholder="Note Text" rows='5' cols='40' value={text} onChange={(event) => setText(event.target.value)}/>
-                </div>
-                <button type="submit" className="btn btn-dark">Submit</button>
-            </form>
-        </div>
+        <NoteContext.Consumer>
+            {context => (
+                <div className="new-note-container">
+                <h3 className="new-note-title">Add New Note</h3>
+                <form className="add-form-container m-3" onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <label>Title</label>
+                        <input type="text" placeholder="Note Title" value={title} onChange={(event) => setTitle(event.target.value)}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Date</label>
+                        <input type="text" placeholder="Node Date" value={date} onChange={(event) => setDate(event.target.value)}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Text</label>
+                        <textarea type="text" placeholder="Note Text" rows='5' cols='40' value={text} onChange={(event) => setText(event.target.value)}/>
+                    </div>
+                    <button type="submit" className="btn btn-dark">Submit</button>
+                </form>
+            </div>
+            )}
+        </NoteContext.Consumer>
     )
 }
 
