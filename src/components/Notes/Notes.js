2faabd04369cefdbd7553f3b6d2a8329705757dd
Ivan Samardzic <isamar01@fesb.hr>
2021-03-11 18:35:06 +0100

    Add an reference to NoteContext and consume all values provided from NoteState

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import './Notes.css'
 import Note from '../Note/Note'
+import NoteContext from '../../context/noteContext'
 
-const Notes = ({notes, deleteNote}) => {
+const Notes = props => {
     return (
-        <div className='notes-box'>
-            {notes.map((note, index) => 
-                <Note note={note}  deleteNote={deleteNote} />
+        <NoteContext.Consumer>
+            {context => (
+                <div className='notes-box'>
+                {context.notes.map((note, index) => 
+                    <Note note={note} />
+                )}
+            </div>
             )}
-        </div>
-    )
-}
+            
+        </NoteContext.Consumer>
+    );
+};
 
 export default Notes
