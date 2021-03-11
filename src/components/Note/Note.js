a724f39b2a16cddb88b4ceb8b48301ed0e4e4e08
Ivan Samardzic <isamar01@fesb.hr>
2021-03-11 18:36:06 +0100

    Add an reference to NoteContext and consume deleteNote method logic

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import './Note.css'
+import NoteContext from '../../context/noteContext'
 
-const Note = ({note, deleteNote}) => {
+const Note = ({note}, props) => {
     return (
-        <div className='note-container' key={note.id}>
-            <i className="fas fa-exclamation-circle"></i>
-            <div className='note-title'>{note.title}</div>
-            <div className='note-date'>{note.date}</div> 
-            <div className='note-text'>{note.text}</div>
-            <button onClick={() => deleteNote(note)} className="note-btn btn btn-light"><i className="far fa-trash-alt"></i></button>
-        </div>
+        <NoteContext.Consumer>
+            {context => (
+                <div className='note-container' key={note.id}>
+                <i className="fas fa-exclamation-circle"></i>
+                <div className='note-title'>{note.title}</div>
+                <div className='note-date'>{note.date}</div> 
+                <div className='note-text'>{note.text}</div>
+                <button onClick={() => context.deleteNote(note)} className="note-btn btn btn-light"><i className="far fa-trash-alt"></i></button>
+            </div>
+            )}
+            
+        </NoteContext.Consumer>
     )
 }
 
